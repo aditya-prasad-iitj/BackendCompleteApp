3f904a7fc78f9aa649dd030e303480b096cb1504
aditya-prasad-iitj <b25ee1002@iitj.ac.in>
2026-07-11 22:48:13 +0530

    Fixed async await in connectDB()

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,8 @@ import { DB_NAME } from "../constants.js";
 async function connectDB(){
 
     try {
-        const connectionInstance = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`Succefully Connected to DB Host ${(await connectionInstance).connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Succefully Connected to DB Host ${(connectionInstance).connection.host}`);
         
         } catch (error) {
         console.log("ERROR Connecting to DB: ", error);
