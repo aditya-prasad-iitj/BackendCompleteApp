b689cb1a9e1c4bcd0475db3ebaa8a8f509913cf9
aditya-prasad-iitj <b25ee1002@iitj.ac.in>
2026-07-12 19:18:47 +0530

    Connecting Express app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import { DB_NAME } from "./constants.js";
 import connectDB from './db/index.js';
 
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("App listening on Port ", process.env.PORT);
+    })
+})
+.catch((err)=>{
+    console.log("Couldn't connect MongoDB: ", err);
+    
+})
 
 /*
 import express from "express"
