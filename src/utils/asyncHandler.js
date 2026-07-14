7430c679e91f6f691d97e579f6893bcf427a2b26
aditya-prasad-iitj <b25ee1002@iitj.ac.in>
2026-07-14 15:41:25 +0530

    Created Users Route & Controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,17 @@
-const asyncHandler = (requestHandler)=>{
+const asyncHandler = (requestHandler)=>
     (req, res, next)=>{
         Promise.resolve(requestHandler(req, res, next))
         .catch((err) => next(err))
-    }
+    
 }
+
+//Same as
+
+// const asyncHandler = (requestHandler)=>{
+//     return (req, res, next)=>{
+//         Promise.resolve(requestHandler(req, res, next))
+//         .catch((err) => next(err))
+//     }
+// }
 
 export {asyncHandler}
