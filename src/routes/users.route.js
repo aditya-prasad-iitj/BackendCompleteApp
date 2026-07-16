f98805b09e9bed9f43c71b78418636c7569712f1
aditya-prasad-iitj <b25ee1002@iitj.ac.in>
2026-07-17 01:20:19 +0530

    Refactor user routes to include account update and media upload functionalities

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,7 +1,20 @@
 import {Router} from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser } from "../controllers/users.controller.js";
+import { registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } from "../controllers/users.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+
 
 const userRouter = Router()
 
@@ -27,5 +40,15 @@ userRouter.route("/refreshAccessToken").post(verifyJWT, refreshAccessToken)
 userRouter.route("/changePassword").post(verifyJWT, changePassword)
 
 userRouter.route("/getCurrentUser").post(verifyJWT, getCurrentUser)
+
+userRouter.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails)
+
+userRouter.route("/avatar").patch(verifyJWT, upload.single("/avatar"), updateUserAvatar)
+
+userRouter.route("/coverImage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+userRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+userRouter.route("/history").get(verifyJWT, getWatchHistory)
 
 export default userRouter
