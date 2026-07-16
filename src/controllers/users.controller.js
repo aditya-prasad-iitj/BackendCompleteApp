3757fa76bfd2475c12f59fb59c1cee3dea7935bc
aditya-prasad-iitj <b25ee1002@iitj.ac.in>
2026-07-17 01:07:38 +0530

    Add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -432,7 +432,59 @@ const getUserChannelProfile = asyncHandler(async(req, res) =>{
     )
 })
 
+const getWatchHistory = asyncHandler(async (req, res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup:{
+                            from: "users",
+                            localField: "_id",
+                            foreignField: "owner",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project:{
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        },
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    },
+                    
+                    
+                ]
+            }
+        }
+    ])
 
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
 
 export {registerUser, 
 loginUser,
@@ -442,5 +494,7 @@ changePassword,
 getCurrentUser,
 updateAccountDetails,
 updateUserAvatar,
-updateUserCoverImage
+updateUserCoverImage,
+getUserChannelProfile,
+getWatchHistory
 }
